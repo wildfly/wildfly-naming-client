327824cd8cd8ee553ded5d475b36102013e2a5bc
Farah Juma <fjuma@redhat.com>
2017-06-01 13:37:05 -0400

    [WFNC-34] Ensure quiet local authentication is disabled if the user hasn't already set the quiet local authentication property and a callback handler or username has been specified in the legacy properties

--- src/main/java/org/wildfly/naming/client/remote/RemoteNamingProviderFactory.java
+++ src/main/java/org/wildfly/naming/client/remote/RemoteNamingProviderFactory.java
@@ -57,10 +57,13 @@ import org.wildfly.naming.client.util.FastHashtable;
 import org.wildfly.security.auth.client.AuthenticationConfiguration;
 import org.wildfly.security.auth.client.AuthenticationContext;
 import org.wildfly.security.auth.client.AuthenticationContextConfigurationClient;
+import org.wildfly.security.sasl.localuser.LocalUserClient;
 import org.wildfly.security.util.CodePointIterator;
 import org.xnio.Option;
 import org.xnio.OptionMap;
 import org.xnio.Options;
+import org.xnio.Property;
+import org.xnio.Sequence;
 
 /**
  * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
@@ -108,7 +111,7 @@ public final class RemoteNamingProviderFactory implements NamingProviderFactory 
         final String passwordBase64 = getProperty(properties, PASSWORD_BASE64_KEY, null, false);
         final String realm = getProperty(properties, REALM_KEY, null, true);
         final OptionMap configuredConnectOptions = getOptionMapFromProperties(properties, CONNECT_OPTIONS_PREFIX, classLoader);
-        final OptionMap connectOptions = mergeWithDefaultOptionMap(DEFAULT_CONNECTION_CREATION_OPTIONS, configuredConnectOptions);
+        OptionMap connectOptions = mergeWithDefaultOptionMap(DEFAULT_CONNECTION_CREATION_OPTIONS, configuredConnectOptions);
 
         /*
          * If this flag is set, then we'll use the "operate" credentials for both connection *and* operation.  If not,
@@ -136,6 +139,11 @@ public final class RemoteNamingProviderFactory implements NamingProviderFactory 
                 throw Messages.log.plainTextAndBase64PasswordSpecified();
             }
             decodedPassword = passwordBase64 != null ? CodePointIterator.ofString(passwordBase64).base64Decode().asUtf8String().drainToString() : password;
+        }
+
+        if (callbackHandler != null || userName != null) {
+            // disable quiet local auth
+            connectOptions = setQuietLocalAuth(connectOptions, false);
         }
 
         AuthenticationContext captured = AuthenticationContext.captureCurrent();
@@ -247,5 +255,32 @@ public final class RemoteNamingProviderFactory implements NamingProviderFactory 
 
     private static ClassLoader getContextClassLoader() {
         return Thread.currentThread().getContextClassLoader();
+    }
+
+    /**
+     * Set the quiet local auth property to the given value if the user hasn't already set this property.
+     *
+     * @param optionMap the option map
+     * @param useQuietAuth the value to set the quiet local auth property to
+     * @return the option map with the quiet local auth property set to the given value if the user hasn't already set this property
+     */
+    private static OptionMap setQuietLocalAuth(final OptionMap optionMap, final boolean useQuietAuth) {
+        final Sequence<Property> existingSaslProps = optionMap.get(Options.SASL_PROPERTIES);
+        if (existingSaslProps != null) {
+            for (Property prop : existingSaslProps) {
+                final String propKey = prop.getKey();
+                if (propKey.equals(LocalUserClient.QUIET_AUTH) || propKey.equals(LocalUserClient.LEGACY_QUIET_AUTH)) {
+                    // quiet local auth property was already set, do not override it
+                    return optionMap;
+                }
+            }
+            // set the quiet local auth property since it wasn't already set in SASL_PROPERTIES
+            existingSaslProps.add(Property.of(LocalUserClient.QUIET_AUTH, Boolean.toString(useQuietAuth)));
+            return optionMap;
+        }
+        // set the quiet local auth property since no SASL_PROPERTIES were set
+        final OptionMap.Builder updatedOptionMapBuilder = OptionMap.builder().addAll(optionMap);
+        updatedOptionMapBuilder.set(Options.SASL_PROPERTIES, Sequence.of(Property.of(LocalUserClient.QUIET_AUTH, Boolean.toString(useQuietAuth))));
+        return updatedOptionMapBuilder.getMap();
     }
 }
